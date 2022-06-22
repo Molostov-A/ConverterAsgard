427fcf337367943e5ba8b2f268f5891453623f71
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-06-22 22:18:40 +0300

    Coords+

--- ConverterAsgard.Common/Models/Coords.cs
+++ ConverterAsgard.Common/Models/Coords.cs
@@ -8,5 +8,11 @@ namespace ConverterAsgard.Common.Models
 {
     public class Coords
     {
+        public int latT { get; set; }
+        public int latN { get; set; }
+        public int latS { get; set; }
+        public int lonT { get; set; }
+        public int lonW { get; set; }
+        public int lonE { get; set; }
     }
 }
