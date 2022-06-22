59564e9a2b412426ded045087bea1436a8c48f3e
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-06-22 22:16:35 +0300

    Вынес Army в отдельный файл

---
+++ ConverterAsgard.Common/Models/Army.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterAsgard.Common.Models
+{
+    public class Army
+    {
+        public string icon { get; set; }
+        public string name { get; set; }
+        public double rural { get; set; }
+        public double urban { get; set; }
+        public int crew { get; set; }
+        public int power { get; set; }
+        public string type { get; set; }
+        public int separate { get; set; }
+    }
+}
--- ConverterAsgard.Common/Models/Options.cs
+++ ConverterAsgard.Common/Models/Options.cs
@@ -22,16 +22,4 @@ namespace ConverterAsgard.Common.Models
         public bool rescaleLabels { get; set; }
         public int urbanDensity { get; set; }
     }
-
-    public class Army
-    {
-        public string icon { get; set; }
-        public string name { get; set; }
-        public double rural { get; set; }
-        public double urban { get; set; }
-        public int crew { get; set; }
-        public int power { get; set; }
-        public string type { get; set; }
-        public int separate { get; set; }
-    }
 }
