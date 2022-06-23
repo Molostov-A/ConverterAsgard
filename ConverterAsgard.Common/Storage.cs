b86eedf26cd29a4261cced876d692f76d60d7355
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-06-23 14:52:27 +0300

    Storage+

---
+++ ConverterAsgard.Common/Storage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using ConverterAsgard.Common.Models;
+
+namespace ConverterAsgard.Common
+{
+    public class Storage
+    {
+        private static JsonProvider storage;
+        private FullMap Map { get; set; }
+
+        public Storage()
+        {
+            storage = new JsonProvider("map_test");
+            Map = storage.Read<FullMap>();
+        }
+
+        public FullMap GetMap()
+        {
+            return Map;
+        }
+
+        public void Update(FullMap result)
+        {
+            Map = result;
+            Save();
+        }
+
+        private void Save()
+        {
+            storage.Write(Map);
+        }
+    }
+}
