8d706d69291fd8f361d6299d5875f2cf27ea9d45
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-06-23 14:40:53 +0300

    Biomes++

--- ConverterAsgard.Common/Models/Biomes.cs
+++ ConverterAsgard.Common/Models/Biomes.cs
@@ -2,11 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ConverterAsgard.Common.Models
 {
     public class Biomes
+    {
+
+        [JsonPropertyName("i")]
+        public int[] Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string[] Name { get; set; }
+
+        [JsonPropertyName("color")]
+        public string[] Color { get; set; }
+
+        [JsonPropertyName("biomesMartix")]
+        public BiomeMatrix[] BiomesMartix { get; set; }
+
+        [JsonPropertyName("habitability")]
+        public int[] Habitability { get; set; }
+
+        [JsonPropertyName("iconsDensity")]
+        public int[] IconsDensity { get; set; }
+
+        [JsonPropertyName("icons")]
+        public string[][] Icons { get; set; }
+    }
+
+    public enum BiomeMatrix
     {
     }
 }
